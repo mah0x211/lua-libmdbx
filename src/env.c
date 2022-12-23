93633b5addc35543c3fbdeacabbd736bf6670a9b
Masatoshi Fukunaga <mah0x211@gmail.com>
2022-12-23 16:54:08 +0900

    Fix bug that the return value of env.sync method

--- src/env.c
+++ src/env.c
@@ -485,7 +485,7 @@ static int sync_lua(lua_State *L)
     switch (rc) {
     case 0:
     case MDBX_RESULT_TRUE:
-        lua_pushboolean(L, 0);
+        lua_pushboolean(L, 1);
         return 1;
 
     default:
